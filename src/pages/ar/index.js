fc57cbcb56e2cb5490cb82e355d3853dff7fec2e
anotherITdude <abhi.baidya@gmail.com>
2023-02-28 18:23:17 +0100

    initial setup

---
+++ src/pages/ar/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+function Ar() {
+    return (
+        <>
+            <Head >
+                <title>Al Ain farms - Cook In The Bag</title>
+                <meta name="description" content="" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <main className={styles.main}>
+
+                <div className="mt-10 text-white">NextJS Ararbic</div>
+
+
+
+
+            </main>
+        </>
+    )
+}
+
+export default Ar
