c8c51a68d578c052f9815a83a485719b3432aafe
anotherITdude <abhi.baidya@gmail.com>
2023-03-02 23:19:18 +0100

    test ignore

--- src/pages/components/carousel.js
+++ src/pages/components/carousel.js
@@ -33,32 +33,7 @@ function Carousel() {
           >Flavours!</motion.p>
         </div>
 
-        <div>
-          <h2> Single Item</h2>
-          {/* <Slider {...settings}>
-            <div className="slide_item">
-              <h3>1</h3>
-            </div>
-            <div className="slide_item">
-              <h3>2</h3>
-            </div>
-            <div className="slide_item">
-              <h3>3</h3>
-            </div>
-            <div className="slide_item">
-              <h3>4</h3>
-            </div>
-            <div className="slide_item">
-              <h3>5</h3>
-            </div>
-            <div className="slide_item">
-              <h3>6</h3>
-            </div>
-            <div className="slide_item">
-              <h3>7</h3>
-            </div>
-          </Slider> */}
-        </div>
+
       </div>
     </div>
   )
