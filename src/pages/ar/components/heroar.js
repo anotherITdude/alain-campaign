3d7493e33d4bc53ba8dbfa47be262a0177330591
anotherITdude <abhi.baidya@gmail.com>
2023-03-20 00:44:20 +0100

    arabic version refactor

---
+++ src/pages/ar/components/heroar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Heroar() {
+    return (
+        <div>
+            <div class="flex flex-col md:flex-row">
+                
+                <div class=" 
+                h-[300px] md:h-[600] 
+                md:basis-45 
+                bg-gradient-to-tr from-[#191919]/60 via-[#191919]/80 to-[#191919]
+                md:bg-gradient-to-r md:from-[#ec6558]/90 md:via-[#ec6558]/75 md:to-[#ec6558]">
+                    02
+                </div>
+                <div class="
+                h-[300px] md:h-[600] 
+                md:basis-55 
+                bg-gradient-to-r from-[#ec6558]/90 via-[#ec6558]/75 to-[#ec6558]
+                md:bg-gradient-to-r md:from-[#191919]/60 md:via-[#191919]/80 md:to-[#191919]">
+                    01
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Heroar
